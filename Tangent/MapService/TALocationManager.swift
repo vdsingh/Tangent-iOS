bf3b1f5c00cff625025c6624667508bd17f5d94a
Vikram Singh <vik.devmail@gmail.com>
2023-03-25 17:48:10 -0400

    Implement LocationManager with functionality such as: Start/Stop Updating Location, Request Authorization, Error Handling, etc.

--- Tangent/MapService/TALocationManager.swift
+++ Tangent/MapService/TALocationManager.swift
@@ -6,3 +6,79 @@
 //
 
 import Foundation
+import CoreLocation
+
+//TODO: Docstring
+protocol TALocationManagerDelegate: CLLocationManagerDelegate {
+    
+    //TODO: Docstring
+    func handleLocationAuthorizationFailure(authorizationStatus: CLAuthorizationStatus)
+}
+
+//TODO: Docstring
+class TALocationManager: Debuggable {
+    
+    let debug = true
+    
+    //TODO: Docstring
+    static let shared = TALocationManager()
+    
+    //TODO: Docstring
+    private var delegate: TALocationManagerDelegate?
+    
+    //TODO: Docstring
+    private let locationManager: CLLocationManager = CLLocationManager()
+        
+    //TODO: Docstring
+    private init() { }
+    
+    //TODO: Docstring
+    private func validateAuthorizationStatus() -> Bool {
+        switch self.locationManager.authorizationStatus {
+        case .denied, .restricted, .notDetermined:
+            return false
+        default:
+            break
+        }
+        
+        return true
+    }
+    
+    // MARK: - Public Functions
+    
+    //TODO: Docstring
+    func setDelegate(delegate: TALocationManagerDelegate) {
+        self.locationManager.delegate = delegate
+        self.delegate = delegate
+    }
+    
+    //TODO: Docstring
+    func requestWhenInUseAuthorization() {
+        self.locationManager.requestWhenInUseAuthorization()
+    }
+    
+    //TODO: Docstring
+    func startUpdatingLocation() {
+        if self.validateAuthorizationStatus() {
+            self.locationManager.startUpdatingLocation()
+            printDebug("Started Updating Location: \(locationManager.requestLocation())")
+        } else {
+            guard let delegate = self.delegate else {
+                print("$ERR: LocationManager Delegate not set")
+                return
+            }
+            
+            delegate.handleLocationAuthorizationFailure(authorizationStatus: locationManager.authorizationStatus)
+        }
+    }
+    
+    //TODO: Docstring
+    func stopUpdatingLocation() {
+        printDebug("Stopped Updating Location")
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func printDebug(_ message: String) {
+        print("$LOG: \(message)")
+    }
+}
