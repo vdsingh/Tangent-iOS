176d8d267eaef5fc734bd38032f3205c87342e8a
Vikram Singh <vik.devmail@gmail.com>
2023-03-25 17:48:40 -0400

    Implement Delegate functionality, Private MKMapView Extension in MapViewController

--- Tangent/Controllers/MapViewController.swift
+++ Tangent/Controllers/MapViewController.swift
@@ -11,11 +11,60 @@ import MapKit
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        TALocationManager.shared.setDelegate(delegate: self)
+        TALocationManager.shared.startUpdatingLocation()
     }
-
-
 }
 
+extension MapViewController: TALocationManagerDelegate {
+    
+    func handleLocationAuthorizationFailure(authorizationStatus: CLAuthorizationStatus) {
+        switch authorizationStatus {
+        case .restricted, .denied:
+            print("$ERR: authorization status failure: \(authorizationStatus)")
+        case .notDetermined:
+            TALocationManager.shared.requestWhenInUseAuthorization()
+        default:
+            print("$ERR: authorization failure called with a successful status: \(authorizationStatus)")
+        }
+    }
+    
+    func locationManager(
+        _ manager: CLLocationManager,
+        didUpdateLocations locations: [CLLocation]
+    ) {
+        if let location = locations.last {
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            print("$LOG: Last user location: (\(latitude), \(longitude))")
+            self.mapView.centerToLocation(CLLocation(latitude: latitude, longitude: longitude))
+            // Handle location update
+        } else {
+            print("$ERR: User locations is empty.")
+        }
+    }
+
+    func locationManager(
+        _ manager: CLLocationManager,
+        didFailWithError error: Error
+    ) {
+        print("$ERR: failed to update location with error: \(String(describing: error))")
+        // Handle failure to get a user’s location
+    }
+}
+
+private extension MKMapView {
+  func centerToLocation(
+    _ location: CLLocation,
+    regionRadius: CLLocationDistance = 1000
+  ) {
+    let coordinateRegion = MKCoordinateRegion(
+      center: location.coordinate,
+      latitudinalMeters: regionRadius,
+      longitudinalMeters: regionRadius)
+    setRegion(coordinateRegion, animated: true)
+  }
+}
